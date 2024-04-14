e18b8aaa40655441a7641f11b6f975dc520c94f4
Walter García <165535934+LcdoWalterGarcia@users.noreply.github.com>
2024-04-14 18:41:49 -0500

    Add files via upload

---
+++ Uqload.php
@@ -0,0 +1,27 @@
+<?php
+$filelink="https://uqload.to/b1v2troqjrxt.html";
+if (strpos($filelink,"uqload.to") !== false || strpos($filelink,"uqload.to") !== false) {
+  if (strpos($filelink,"https") === false) $filelink=str_replace("http","https",$filelink);
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $filelink);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 5.1; rv:22.0) Gecko/20100101 Firefox/22.0');
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION  ,1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+  $h2 = curl_exec($ch);
+  curl_close($ch);
+  if (preg_match('/([http|https][\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.mp4))/', $h2, $m)) {
+    $link=$m[1];
+    $link=str_replace("https","http",$link);
+  } else
+    $link="";
+  if (preg_match('/([http|https]?[\.\d\w\-\.\/\\\:\?\&\#\%\_\,]*(\.(srt|vtt)))/', $h2, $m)) {
+   $srt=$m[1];
+   if (strpos($srt,"http") === false) $srt="https:".$srt;
+   if (strpos($srt,"empty") !== false) $srt="";
+  }
+}
+echo $link;
+?>
